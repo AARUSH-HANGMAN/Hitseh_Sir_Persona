50cd7b76bde8b43e8dff8cf75448a98b29910fd1
Aarush Padha <aarushpadha@Aarushs-MacBook-Air.local>
2026-02-15 20:12:26 +0530

    added typing animation effect

--- app.py
+++ app.py
@@ -105,6 +105,17 @@ st.markdown("""
 """)
 
 
+
+# Display OLD messages first
+for msg in st.session_state.messages[1:]:
+    with st.chat_message(
+        msg["role"],
+        avatar="Assets/hitesh.png" if msg["role"]=="assistant" else "Assets/student.png"
+    ):
+        st.markdown(msg["content"])
+
+
+# Then take new input
 user_input = st.chat_input("Ask something...")
 
 if user_input:
@@ -126,10 +137,3 @@ if user_input:
         typing_placeholder.markdown(reply)
 
     st.session_state.messages.append({"role": "assistant", "content": reply})
-
-for msg in st.session_state.messages[1:]:
-    with st.chat_message(
-        msg["role"],
-        avatar="Assets/hitesh.png" if msg["role"]=="assistant" else "Assets/student.png"
-    ):
-        st.markdown(msg["content"])
